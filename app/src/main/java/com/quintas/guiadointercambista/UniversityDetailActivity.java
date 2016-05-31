d06e03166bb210d12c6117a6b584f3b6ed184f06
Jose C Quintas Jr <juniorquintas@gmail.com>
2016-05-31 00:01:45 -0300

    Melhoras em vários layouts

--- app/src/main/java/com/quintas/guiadointercambista/UniversityDetailActivity.java
+++ app/src/main/java/com/quintas/guiadointercambista/UniversityDetailActivity.java
@@ -103,7 +103,7 @@ public class UniversityDetailActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_university_detail, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_university_detail_info, container, false);
 //            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
 //            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
